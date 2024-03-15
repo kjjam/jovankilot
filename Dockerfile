1b135fabbdfe373cb88cf637473bbce1371664ce
ghofloyaragh <jkeyvan@gmail.com>
2024-03-15 21:55:29 +0330

    changes to allowed host

--- Dockerfile
+++ Dockerfile
@@ -7,6 +7,7 @@ ENV PYTHONUNBUFFERED 1
 
 WORKDIR /app
 
+RUN pip install --upgrade pip
 COPY ./requirements.txt /app
 RUN pip install --no-cache-dir -r requirements.txt
 
--- javan_locksmith/settings.py
+++ javan_locksmith/settings.py
@@ -26,7 +26,7 @@ WEB_ID_SALT = '$2b$12$0LW60C5.14vkQ5A0MAw6rO'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', False) == 'True'
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["*","javankelid.darkube.app"]
 
 # Application definition
 
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -117,6 +118,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
